ef4c90c6de145786eeb7eb0909f823fb89c596ea
Echo Lee <bdtoon.lee+account.git@gmail.com>
2015-10-16 17:49:50 +0800

    View initial commit

---
+++ View/EchoHelper/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace EchoHelper
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ View/EchoHelper/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace EchoHelper
+{
+    static class Program
+    {
+        static string assemblyGuid
+        {
+            get
+            {
+                object[] attributes =
+                    Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(GuidAttribute), false);
+
+                if (attributes.Length == 0)
+                {
+                    return string.Empty;
+                }
+                else
+                {
+                    return ((GuidAttribute)attributes[0]).Value;
+                }
+            }
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            using (Mutex mutex = new Mutex(false, @"Global\" + assemblyGuid))
+            {
+                if (mutex.WaitOne(0, false) == false)
+                {
+                    MessageBox.Show("EchoHelper is running !");
+                }
+                else
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new MainForm());
+                }
+            }
+        }
+    }
+}
